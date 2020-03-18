c9d5ff4c202b2399c4a6728a8ffc7e3eef29a8eb
折田幸夫 <3204704@gmail.com>
2020-03-18 23:50:03 +0900

    update

--- ruby_projects/index.rb
+++ ruby_projects/index.rb
@@ -1,1 +1,17 @@
-puts "Hello World !"
+# puts "Hello World !"
+# puts 10 + 9
+
+name = nil
+
+puts name
+
+# これは原則使わない
+i,j = 2,4
+puts i
+puts j
+
+languages = ["日本語","英語","スペイン語"]
+puts languages
+
+puts 3 / 2
+puts 3.0 / 2
