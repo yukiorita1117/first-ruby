20b54f0badb84c476abe6f4c17acb48e0413fb22
折田幸夫 <3204704@gmail.com>
2020-03-19 00:02:14 +0900

    update

--- ruby_projects/index.rb
+++ ruby_projects/index.rb
@@ -15,3 +15,22 @@ puts languages
 
 puts 3 / 2
 puts 3.0 / 2
+
+# 小数点にする
+f_num = 3 / 2
+puts f_num.to_f
+
+puts 1.class
+puts languages.class
+
+puts "私は\nエンジニアです。"
+
+first_name = "JSONハードコーダー"
+
+puts "#{first_name}です"
+
+s = "aaaaa"
+puts s.upcase
+puts s
+# 破壊的メソッド 内容が永続的に変わるもの
+puts s.upcase!
