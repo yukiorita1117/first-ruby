4da09e2c376ac21fa2f53605c67894304783c9a1
折田幸夫 <3204704@gmail.com>
2020-03-21 04:41:58 +0900

    add exception

---
+++ ruby_projects/exception.rb
@@ -0,0 +1,20 @@
+puts "--------数値を入力してください--------"
+
+# ターミナルからの値取得(integerで)
+i = gets.to_i
+
+
+# 
+begin
+    # 例外が起きうる処理
+    puts 10 / i
+rescue => ex
+    # 例外が発生した時の処理書く
+    puts "errorになったよ"
+    puts ex.message
+    puts ex.class #例外のクラス名
+ensure
+    # 例外の発生の有無にかかわらず通る処理
+    puts "終了"
+
+end
