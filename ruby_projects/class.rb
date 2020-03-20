474827e782b78ccad397a39b8e02c2f6543de41f
折田幸夫 <3204704@gmail.com>
2020-03-21 04:14:38 +0900

    class done

--- ruby_projects/class.rb
+++ ruby_projects/class.rb
@@ -1,24 +1,35 @@
 class User
+    # 定数
+    REGION = "Tokyo"
+    # クラス変数
+    @@count = 0
+
     # アクセサメソッド
-    attr_accessor :name
+    # attr_accessor :name
     # attr_accessor :age
 
     # 変更されたくないものは読み取り専用でかく
-    attr_reader :age
+    # attr_reader :age
 
     # ちなみに出力はしたくないが変更はしたい場合は書き込み専用で
     # attr_writer :name
 
     # initializeメソッド
-    def initialize(name,age)
+    def initialize(name)
         # initialize変数 Userクラス内でならどこでも使える
         @name = name
-        @age = age
+        @@count += 1
+        # @age = age
     end
 
     # メソッド
     def hello
-        puts "おはよう#{@name}"
+        puts "おはよう#{@name}.#{@@count}instance(s)"
+    end
+
+    # クラスメソッド
+    def self.info
+        puts "クラスメソッドだよ。#{@@count}:instance(s).#{REGION}"
     end
 end
 
@@ -34,9 +45,22 @@ end
 
 # アクセサ
 # class内のインスタンス変数を読み込み、書き変えることができる
-user = User.new("yukio",26)
+# user = User.new("yukio",26)
 
-puts user.name #アクセサなしだとerror
-# puts user.name = "nobuo"
+# puts user.name #アクセサなしだとerror
+# # puts user.name = "nobuo"
 
-puts user.age = 100 #errorになる
+# puts user.age = 100 #errorになる
+
+
+# 
+# yukio = User.new("yukio")
+# yukio.hello
+nobuo = User.new("nobuo")
+nobuo.hello
+
+# クラスメソッドはインスタンスなしでも呼べる
+User.info
+
+# クラス内の定数もインスタンスなしで呼べる
+puts User::REGION
