9becd344a45412ce2525580fac9bc5452edc1765
折田幸夫 <3204704@gmail.com>
2020-03-20 23:13:43 +0900

    update

--- ruby_projects/second.rb
+++ ruby_projects/second.rb
@@ -2,4 +2,12 @@ puts "start!!!"
 
 # rubyならではの範囲オブジェクト
 # to_aのaはarray　つまり配列化と言うこと
-puts (1..25).to_a
+# puts (1..25).to_a.shuffle
+
+# 配列に要素を追加する
+a = (1..10).to_a
+p a
+p a << 20
+puts "-------------"
+p a.push(30)
+# pでなく、putsだと配列を展開してしまう
