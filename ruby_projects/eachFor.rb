58d6c7e28b96e2c646570096594199be5f2fabcc
折田幸夫 <3204704@gmail.com>
2020-03-21 02:34:12 +0900

    step

--- ruby_projects/eachFor.rb
+++ ruby_projects/eachFor.rb
@@ -1,22 +1,41 @@
-numbers = [1,2,3,4,5]
+# numbers = [1,2,3,4,5]
 
-# eachで取り出す
-# ||~ はjsで言う所の ()=>{}かな
-numbers.each { |number| puts number}
+# # eachで取り出す
+# # ||~ はjsで言う所の ()=>{}かな
+# numbers.each { |number| puts number}
 
-# ちなみに連想配列でも変わらない
-person = {name: "yukio", age: 26}
+# # ちなみに連想配列でも変わらない
+# person = {name: "yukio", age: 26}
 
-person.each { |key,value| puts "#{key}","#{value}" }
+# person.each { |key,value| puts "#{key}","#{value}" }
 
-# 冗長にも勿論かける
-# numbers.each do |number|
-#     puts number
-# end
-puts "-----------"
+# # 冗長にも勿論かける
+# # numbers.each do |number|
+# #     puts number
+# # end
+# puts "-----------"
 
-# forで取り出す が 原則forは使わない！！！！
-# for number in numbers do
-#     puts number
-# end
+# # forで取り出す が 原則forは使わない！！！！
+# # for number in numbers do
+# #     puts number
+# # end
 
+# times
+# 配列を使わずに繰り返し処理ができる
+
+5.times do |i|
+    puts "#{i}: ああああ"
+end
+
+# jsのアロー的に
+5.times { |i| puts "#{i}:いいいいいい"}
+
+3.times { |index| puts "#{index}"}
+
+# while
+j = 0
+
+while j < 10
+    puts "#{j}まだ10回じゃないよ"
+    j += 1
+end
---
+++ ruby_projects/uptoDownto.rb
@@ -0,0 +1,8 @@
+# 10.upto(19) { |n| puts n}
+# 10.downto(-9) { |n| puts n}
+
+
+# step
+# step(終了位置,stepする度合い)
+# 10.step(100,10) { |n| puts n}
+10.step(-100,-20) { |n| puts n}
