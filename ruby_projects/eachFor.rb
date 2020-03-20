cddc4b83ce9a236a0e233949056a546736f94826
折田幸夫 <3204704@gmail.com>
2020-03-21 02:18:12 +0900

    loop

--- ruby_projects/eachFor.rb
+++ ruby_projects/eachFor.rb
@@ -1,6 +1,22 @@
-number = [1,2,3,4,5]
+numbers = [1,2,3,4,5]
 
 # eachで取り出す
+# ||~ はjsで言う所の ()=>{}かな
+numbers.each { |number| puts number}
 
+# ちなみに連想配列でも変わらない
+person = {name: "yukio", age: 26}
 
-# forで取り出す
+person.each { |key,value| puts "#{key}","#{value}" }
+
+# 冗長にも勿論かける
+# numbers.each do |number|
+#     puts number
+# end
+puts "-----------"
+
+# forで取り出す が 原則forは使わない！！！！
+# for number in numbers do
+#     puts number
+# end
+
